1562dc9129ccb7a8f35f7cacc763b6772bc86c16
tiago-rodrigues1 <tiagords127@gmail.com>
2026-05-10 14:26:38 -0300

    feat: add menu component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MapComponent } from "@/components/Map/MapComponent";
+import { MenuComponent } from "@/components/Menu/MenuComponent";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<MenuComponent />
 			<Routes>
 				<Route path="/" element={<MapComponent />} />
+				{/* <Route path="/ocurrence/new" /> */}
 			</Routes>
 		</BrowserRouter>
 	);
