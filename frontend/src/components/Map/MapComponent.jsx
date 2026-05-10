56962125dbcb76a99dd21ac2fda8052d81084489
tiago-rodrigues1 <tiagords127@gmail.com>
2026-05-10 14:25:34 -0300

    refactor: changes zoom control position

--- frontend/src/components/Map/MapComponent.jsx
+++ frontend/src/components/Map/MapComponent.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, ZoomControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export const MapComponent = () => {
@@ -6,14 +6,16 @@ export const MapComponent = () => {
 		<div className="h-screen w-full">
 			<MapContainer
 				center={{ lat: -5.832052, lng: -35.2080847 }}
-				zoom={16}
-				scrollWheelZoom={false}
+				zoom={13}
 				className="h-full w-full"
+				zoomControl={false}
 			>
+				<Marker position={{ lat: -5.832052, lng: -35.2080847 }} />
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
+				<ZoomControl position="bottomright" />
 			</MapContainer>
 		</div>
 	);
